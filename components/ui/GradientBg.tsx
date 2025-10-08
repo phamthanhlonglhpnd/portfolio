30aa1ca735c5cf984999c9a2da8c51d5d1258353
Sotatek-LongPham2 <long.pham2@sotatek.com>
2025-10-08 23:05:42 +0700

    fix bug build failed

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -56,7 +56,18 @@ export const BackgroundGradientAnimation = ({
     document.body.style.setProperty("--pointer-color", pointerColor);
     document.body.style.setProperty("--size", size);
     document.body.style.setProperty("--blending-value", blendingValue);
-  }, []);
+  }, [
+    blendingValue,
+    fifthColor,
+    firstColor,
+    fourthColor,
+    gradientBackgroundEnd,
+    gradientBackgroundStart,
+    pointerColor,
+    secondColor,
+    size,
+    thirdColor,
+  ]);
 
   useEffect(() => {
     function move() {
